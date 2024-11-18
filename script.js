7d609090ee08f1f25e01d25fb5c1c3b3231b4633
gab07099 <samontegabriel1979@gmail.com>
2024-11-18 19:53:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -86,17 +86,5 @@ function getArtistSongs(artist) {
 } 
 
 
-<script>
-document.querySelector('.feedback-form').addEventListener('submit', function(event) {
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const message = document.getElementById('message').value.trim();
 
-    if (!name || !email || !message) {
-        alert('Please fill in all fields.');
-        event.preventDefault(); // Prevent form submission
-    } else {
-        alert('Thank you for your feedback!');
-    }
-});
-</script>
+
